03fbc3a3f5fedfa630f647c6e9c4135588bdf4fc
peter-abada <peter_abada@dlsu.edu.ph>
2025-04-01 22:34:55 +0800

    github stupidity

--- GDADPRG-MP/main.cpp
+++ GDADPRG-MP/main.cpp
@@ -1,9 +1,26 @@
-#include "Game.h"
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    Game game;
-    game.run();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    //Test push
+
     return 0;
 }
